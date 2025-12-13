0f62d0b3777dfdb8219604b0d1482b01e148a323
unknown <lukas.strnad222@gmail.com>
2025-12-13 04:20:22 +0100

    added carfittery

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { Atf } from './atf/atf';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Atf],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
