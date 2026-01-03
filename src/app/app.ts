af5290648740990fda6b73ad48ef64c0559c5c12
unknown <lukas.strnad222@gmail.com>
2026-01-04 00:03:19 +0100

    added services component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Atf } from './atf/atf';
+import { Services } from './services/services';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Atf],
+  imports: [RouterOutlet, Header, Atf, Services],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
