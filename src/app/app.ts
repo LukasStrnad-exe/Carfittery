97446f75ca877d7a3dd8b48cac06b7599c164d0b
unknown <lukas.strnad222@gmail.com>
2026-01-04 00:33:29 +0100

    added maps

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Atf } from './atf/atf';
 import { Services } from './services/services';
+import { Maps } from './maps/maps';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Atf, Services],
+  imports: [RouterOutlet, Header, Atf, Services, Maps],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
